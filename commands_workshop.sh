5e3824ce8aa22596ec87926afe37ec4f2ca96193
Ubuntu <ubuntu@ip-172-31-21-158.eu-central-1.compute.internal>
2020-07-29 15:14:48 +0000

    updated commands workshop

--- commands_workshop.sh
+++ commands_workshop.sh
@@ -1,30 +1,31 @@
 #!/usr/bin/env bash
 #
-# NanoPlot \
-# --fastq /data/reads/lrrnaseq/parietal_cortex-5238-batch1.fastq.gz \
-# --N50 \
-# --prefix parietal_cortex-5238-batch1_ \
-# -o ~/nanoplot
-#
-# nanoQC \
-# -o ~/nanoplot \
-# /data/reads/lrrnaseq/parietal_cortex-5238-batch1.fastq.gz
+NanoPlot \
+--fastq /data/reads/lrrnaseq/cerebellum-5238-batch2.fastq.gz \
+--N50 \
+--prefix parietal_cortex-5238-batch1_ \
+-o ~/nanoplot
+
+fastqc \
+/data/reads/lrrnaseq/cerebellum-5238-batch2.fastq.gz \
+-o ~/fastqc
 
 ## approx. 10 minutes on 3 cores
-# minimap2 \
-# -a \
-# -x splice \
-# -G 500k \
-# /data/references/GRCh38.p13.chr12.fa \
-# /data/reads/lrrnaseq/parietal_cortex-5238-batch1.fastq.gz \
-# | samtools sort \
-# | samtools view -bh > ~/parietal_cortex-5238-batch1.bam
-#
-# samtools index ~/parietal_cortex-5238-batch1.bam
+minimap2 \
+-a \
+-x splice \
+-G 500k \
+/data/references/GRCh38.p13.chr12.fa \
+/data/reads/lrrnaseq/parietal_cortex-5238-batch1.fastq.gz \
+| samtools sort \
+| samtools view -bh > ~/parietal_cortex-5238-batch1.bam
+
+samtools index ~/parietal_cortex-5238-batch1.bam
 
 ## start: 11:11
 mkdir ~/read_alignment
 
+## takes about three hours..
 minimap2 \
 -a \
 -x splice \
@@ -38,34 +39,31 @@ samtools index ~/read_alignment/CACNA1C_combined.bam
 
 # mkdir ~/flair_output
 #
-# python3 ~/flair/bin/bam2Bed12.py \
-# -i ~/read_alignment/CACNA1C_combined.bam \
-# > ~/flair_output/CACNA1C_combined.bed12
+python3 ~/flair/bin/bam2Bed12.py \
+-i ~/read_alignment/CACNA1C_combined.bam \
+> ~/flair_output/CACNA1C_combined.bed12
 #
-# python3 ~/flair/flair.py correct \
-# -q ~/flair_output/CACNA1C_combined.bed12 \
-# -g /data/references/GRCh38.p13.chr12.fa \
-# -f /data/references/Homo_sapiens.GRCh38.100.gtf \
-# -o ~/flair_output/CACNA1C 2> ~/logs/flair_correct.log
+python3 ~/flair/flair.py correct \
+-q ~/flair_output/CACNA1C_combined.bed12 \
+-g /data/references/GRCh38.p13.chr12.fa \
+-f /data/references/Homo_sapiens.GRCh38.100.gtf \
+-o ~/flair_output/CACNA1C
 #
-# READS=`ls /data/reads/lrrnaseq/batch_combined/*.fastq.gz | tr "\n" ","`
-# READS="${READS%?}" #remove last comma
+READS=`ls /data/reads/lrrnaseq/batch_combined/*.fastq.gz | tr "\n" ","`
+READS="${READS%?}" #remove last comma
+
+python3 ~/flair/flair.py collapse \
+-g /data/references/GRCh38.p13.chr12.fa \
+-r $READS \
+-q ~/flair_output/CACNA1C_all_corrected.bed \
+-f /data/references/Homo_sapiens.GRCh38.100.gtf \
+-s 3 \
+-t 3 \
+-o ~/flair_output/CACNA1C.collapse
 #
-# python3 ~/flair/flair.py collapse \
-# -g /data/references/GRCh38.p13.chr12.fa \
-# -r $READS \
-# -q ~/flair_output/CACNA1C_all_corrected.bed \
-# -f /data/references/Homo_sapiens.GRCh38.100.gtf \
-# -s 3 \
-# -t 4 \
-# -o ~/flair_output/CACNA1C.collapse \
-# --keep_intermediate \
-# 2> ~/logs/flair_collapse.log
-#
-# cd ~/flair_output
-#
-# python3 ~/flair/flair.py quantify \
-# -r /data/reads/lrrnaseq/batch_combined/reads_manifest.tsv \
-# -i ~/flair_output/CACNA1C.collapse.isoforms.fa \
-# -t 4 \
-# 2> ~/logs/flair_quantify.log
+cd ~/flair_output
+
+python3 ~/flair/flair.py quantify \
+-r /data/reads/lrrnaseq/batch_combined/reads_manifest.tsv \
+-i ~/flair_output/CACNA1C.collapse.isoforms.fa \
+-t 4
